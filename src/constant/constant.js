d8ae23c2aa4d6a759f1ba8293e373e15cabffe04
gnanamoorthy <gnanamoorthyvikey@gmail.com>
2023-08-13 00:58:55 +0530

    igst added

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,6 +1,6 @@
 const api = {
-  //baseUrl: "https://api.vishakamedical.in/v1/",
-  baseUrl: "http://localhost:8080/v1/",
+  baseUrl: "https://api.vishakamedical.in/v1/",
+  //baseUrl: "http://localhost:8080/v1/",
 };
 
 export { api };
