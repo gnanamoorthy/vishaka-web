26990734af4f877c71f57e294afb6ee769390f83
gnanamoorthy <gnanamoorthyvikey@gmail.com>
2023-12-31 23:36:05 +0530

    payment paid page added

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,6 +1,6 @@
 const api = {
-  //baseUrl: "https://api.vishakamedical.in/v1/",
-  baseUrl: "http://localhost:8080/v1/",
+  baseUrl: "https://api.vishakamedical.in/v1/",
+  //baseUrl: "http://localhost:8080/v1/",
 };
 
 const gridStyle = { minHeight: 550, marginTop: 10 };
